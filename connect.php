443ee2483ba30119e858ec185c04e3c131d5cf48
kigo03 <91307743+kigo03@users.noreply.github.com>
2024-06-15 12:41:21 +0530

    Add files via upload

---
+++ connect.php
@@ -0,0 +1,34 @@
+<?php
+
+$servername = "localhost"; 
+$username = "root"; 
+$password = ""; 
+$database = "phoenix";
+
+
+$conn = new mysqli($servername, $username, $password, $database);
+
+
+if ($conn->connect_error) {
+    die("Connection failed: " . $conn->connect_error);
+}
+
+
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+    $name = $_POST["name"];
+    $email = $_POST["email"];
+    $message = $_POST["message"];
+
+    
+    $sql = "INSERT INTO contact (name, email, message) VALUES ('$name', '$email', '$message')";
+
+    if ($conn->query($sql) === TRUE) {
+        echo "New record created successfully";
+    } else {
+        echo "Error: " . $sql . "<br>" . $conn->error;
+    }
+}
+
+
+$conn->close();
+?>
